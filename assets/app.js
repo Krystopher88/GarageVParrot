d30d4d590c717938b40f1b92d3b3695ee749241b
Krystopher88 <bichon.christ@gmail.com>
2023-09-01 12:40:21 +0200

    Template front index header&footer

--- assets/app.js
+++ assets/app.js
@@ -6,4 +6,13 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import "./styles/app.scss";
+
+
+// Menu burger
+$(document).ready(function () {
+  // Activer le menu burger
+  $('.navbar-toggler').on('click', function () {
+    $('.navbar-collapse').toggleClass('show');
+  });
+});
