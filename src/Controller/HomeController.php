6b1476f9cea767e93e63bc85c5235f11f4ac47be
Krystopher88 <bichon.christ@gmail.com>
2023-09-06 12:28:16 +0200

    Function: Contact form V1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
+use App\Form\ModalContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\OpeningSheduleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(OpeningSheduleRepository $openingSheduleRepository): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function index(OpeningSheduleRepository $openingSheduleRepository, Request $request): Response
+    {
+        $form = $this->createForm(ModalContactType::class);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $this->entityManager->persist($form->getData());
+                $this->entityManager->flush();
+            }
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'openingShedules' => $openingSheduleRepository->findAll(),
+            'form' => $form->createView(),
         ]);
     }
 }
